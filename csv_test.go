7621c87c1c813299aa93fcb4dfa681d807fe5907
Konboi <ryosuke.yabuki@gmail.com>
2016-12-07 23:11:59 +0900

    Add tsv test

--- csv_test.go
+++ csv_test.go
@@ -7,14 +7,31 @@ import (
 
 func TestGetDataFromCSV(t *testing.T) {
 	fx := FixtureLoader{}
+	columns := []string{"id", "name"}
+	rows := []map[string]string{map[string]string{"id": "1", "name": "エクスカリバー"}, map[string]string{"id": "2", "name": "村正"}}
+
 	t.Run("load csv", func(t *testing.T) {
 		file := "_data/item.csv"
 		data, err := fx.getDataFromCSV(file, "csv")
 		if err != nil {
 			t.Fatalf("[error] get data from csv: %v", err)
 		}
-		columns := []string{"id", "name"}
-		rows := []map[string]string{map[string]string{"id": "1", "name": "エクスカリバー"}, map[string]string{"id": "2", "name": "村正"}}
+
+		if !reflect.DeepEqual(data.columns, columns) {
+			t.Fatalf("[error] get data from csv: expect: %v but %v", columns, data.columns)
+		}
+
+		if !reflect.DeepEqual(data.rows, rows) {
+			t.Fatalf("[error] get data from csv: expect: %v but %v", data.rows, rows)
+		}
+	})
+
+	t.Run("load tsv", func(t *testing.T) {
+		file := "_data/item.tsv"
+		data, err := fx.getDataFromCSV(file, "tsv")
+		if err != nil {
+			t.Fatalf("[error] get data from csv: %v", err)
+		}
 
 		if !reflect.DeepEqual(data.columns, columns) {
 			t.Fatalf("[error] get data from csv: expect: %v but %v", columns, data.columns)
